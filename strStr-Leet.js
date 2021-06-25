957b5deca80ad44f2bdc09fd9358d30c05ff0f4f
Mason Hoffman <mhoffman3939@gmail.com>
2021-06-25 08:27:41 -0700

    Completed strStr

---
+++ strStr-Leet.js
@@ -0,0 +1,7 @@
+//Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
+
+console.log(strStr('hello', 'll'))  //2
