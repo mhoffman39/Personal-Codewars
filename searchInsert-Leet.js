554dd9996344149d0f255ddb7d0a06f4d2f208c7
Mason Hoffman <mhoffman3939@gmail.com>
2021-06-25 08:48:46 -0700

    Completed searchInsert

---
+++ searchInsert-Leet.js
@@ -0,0 +1,14 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+var searchInsert = function(nums, target) {
+  for (let i = 0; i <= nums.length; i ++) {
+    if (nums[i] >= target) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+console.log(searchInsert([1,3,5,6], 7))
