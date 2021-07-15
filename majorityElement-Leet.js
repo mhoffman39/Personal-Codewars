39fa5e976e8af6211f4640918e449b82a106cab5
Mason Hoffman <mhoffman3939@gmail.com>
2021-07-15 11:11:48 -0500

    Completed majorityElement

---
+++ majorityElement-Leet.js
@@ -0,0 +1,17 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+var majorityElement = function(nums) {
+  //holder object
+  const counter = {};
+  //iterate over the array
+  for (let i = 0; i < nums.length; i++) {
+    counter[nums[i]] >= 1 ? counter[nums[i]]++ : counter[nums[i]] = 1;
+    if (counter[nums[i]] >= nums.length / 2) {
+      return nums[i];
+    }
+  }
+};
+
+console.log(majorityElement([2,2,1,1,1,2,2]))  //2
