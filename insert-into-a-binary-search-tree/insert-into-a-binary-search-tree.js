e820f281c84fed1722527879107aec247c674738
Mason Hoffman <mhoffman3939@gmail.com>
2021-08-17 16:19:04 -0700

    Time: 132 ms (22.98%), Space: 47.6 MB (13.66%) - LeetHub

---
+++ insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+  if (root === null) {return new TreeNode(val)};
+  
+  let current = root;
+  while (true) {
+    if (current.val < val) {
+      if (current.right) {
+        current = current.right;
+      } else {
+        current.right = new TreeNode(val);
+        break;
+      }
+    } else {
+      if (current.left) {
+        current = current.left;
+      } else {
+        current.left = new TreeNode(val);
+        break;
+      }
+    }
+    //return root;
+  }
+  return root;
+};
