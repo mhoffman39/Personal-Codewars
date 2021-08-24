da3bfb01489288d6a768cfc1240822f4bee0d4c9
Mason Hoffman <mhoffman3939@gmail.com>
2021-08-24 14:18:07 -0700

    Time: 60 ms (99.12%), Space: 39.8 MB (85.60%) - LeetHub

--- same-tree/same-tree.js
+++ same-tree/same-tree.js
@@ -19,6 +19,6 @@ var isSameTree = function(p, q) {
   } else if (p.val !== q.val) {
     return false;
   } else {
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    return isSameTree(p.right,q.right) && isSameTree(p.left, q.left)
   }
 };
